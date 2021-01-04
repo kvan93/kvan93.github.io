f60335e926cf22d24b7c07990f3d53157c32d446
K_van93 <kevin.vanaerschodt@gmail.com>
2021-01-04 21:45:43 +0100

    Change font-size and scroll bar_bg on scroll

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 });
 
-
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $("nav.navbar");
+    var $navbrand = $("a.navbar-brand");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $navbrand.height());
+  });
+});
 
 
