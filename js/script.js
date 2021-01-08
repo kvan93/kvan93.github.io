bbf68a783b0ae5605b8cf8194b1cde55fdf43b04
K_van93 <kevin.vanaerschodt@gmail.com>
2021-01-08 16:22:23 +0100

    Adding project_travel + small fixes

--- js/script.js
+++ js/script.js
@@ -7,6 +7,15 @@ $(function () {
     var $nav = $("nav.navbar");
     var $navbrand = $("a.navbar-brand");
     $nav.toggleClass('scrolled', $(this).scrollTop() > $navbrand.height());
+
+    var offsetTop = $("#profileparallax").offset().top + ($(window).height()/2);
+    var scrollTop = $(window).scrollTop();
+    if (scrollTop > offsetTop){
+      $("li#HomeTab").fadeIn(500);
+    } else{
+      $("li#HomeTab").fadeOut(100);
+
+    }
   });
 });
 
